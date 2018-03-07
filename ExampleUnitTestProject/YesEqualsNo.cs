781fb9c7f117b38aee4ad9643bcd778cd7366c3e
Tamesh Sivaguru <tam.s89@gmail.com>
2018-03-07 15:14:29 +0000

    Initial commit

---
+++ ExampleUnitTestProject/YesEqualsNo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleUnitTestProject
+{
+    [TestClass]
+    public class YesEqualsNo
+    {
+        [TestMethod]
+        public void That_Yes_Does_Equal_No()
+        {
+            var yes = "Yes";
+            var no = yes;
+
+            Assert.AreEqual(yes, no);
+        }
+    }
+}
